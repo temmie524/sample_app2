bcbe3fe2dcfb2f489fb882dd60f56ab6623ac228
temmie524 <amano.zyaku55@gmail.com>
2024-03-31 01:17:13 +0900

    Add user microposts

---
+++ test/models/micropost_test.rb
@@ -0,0 +1,34 @@
+require "test_helper"
+
+class MicropostTest < ActiveSupport::TestCase
+  def setup
+    @user = users(:michael)
+    # このコードは慣習的に正しくない
+    # 代わりに、userの関連付けを使ってmicropostを作成するべき
+    # 👇修正
+    @micropost = @user.microposts.build(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  test "comntent should be present" do
+    @micropost.content = "   "
+    assert_not @micropost.valid?
+  end
+
+  test "content should be at most 140 characters" do
+    @micropost.content = "a" * 141
+    assert_not @micropost.valid?
+  end
+
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first
+  end
+end
